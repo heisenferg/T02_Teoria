c53ec1a5ba03dbc1fe759129768e71dbbe366488
heisenferg <heisenferg@gmail.com>
2021-11-10 12:18:08 +0100

    1

--- app/src/main/java/com/example/t02_teoria/MainActivity.java
+++ app/src/main/java/com/example/t02_teoria/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.t02_teoria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioGroup radio = (RadioGroup) findViewById(R.id.radioGroup);
+        radio.setOnCheckedChangeListener(this);
+
+    }
+
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        TextView text = findViewById(R.id.textView);
+
+        switch (checkedId){
+            case R.id.rbTalavera: text.setText("Talavera no es buena opción");
+            break;
+            case R.id.radioButton2: text.setText("Real Madrid es el mejor.");
+            break;
+            case R.id.radioButton3: text.setText("EL Mónaco apesta");
+            break;
+        }
     }
 }
